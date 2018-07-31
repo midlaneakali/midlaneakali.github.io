e51cbe787ad05459597b6f4fb0ddd8caa5ee84bf
Ephemeral <ephemeral@Ephemerals-MacBook-Pro.local>
2018-07-31 23:52:17 +0100

    Colour change

--- Canvas.js
+++ Canvas.js
@@ -52,11 +52,11 @@ function DrawGameboard() {
 
 function DrawRectangle(XPosition, YPosition) {
     rc.rectangle(XPosition, YPosition, GameboardCell.width, GameboardCell.height, {
-        fill: "#36393F",
+        fill: "#AED6F1",
         roughness: 2.5,
         bowing: 1,
          // solid fill
-        fillStyle: "solid"
+        //fillStyle: "solid"
     });
 }
 function MineHit(XPosition,YPosition,MyScore)
