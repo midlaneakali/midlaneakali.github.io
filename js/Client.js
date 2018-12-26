219535ca78eed3b9185ddd14ac3f05133b25f9c7
Ephemeral <ephemeral@Ephemerals-MacBook-Pro.local>
2018-12-26 20:25:57 +0000

    First changes to updated client; no ssl; prints and sends x and y coordinate..it should

---
+++ js/Client.js
@@ -0,0 +1,37 @@
+var ws;
+var MyPlayerId;
+
+function Connect() {
+  //
+  if ("WebSocket" in window) {
+
+
+    ws = new WebSocket("ws://cynosure.pw:8080");
+
+    ws.onopen = function () {
+
+        new Game('beginner');
+        ws.send("hello world");
+    };
+    //RecordingStream test! 
+    ws.onmessage = function (evt) {
+      var received_msg = evt.data;
+        console.log(received_msg);
+    
+
+    };
+
+    ws.onclose = function () {
+      Live = false;
+
+      alert("Server offline try again soon!");
+    };
+  } else {
+
+    // The browser doesn't support WebSocket
+
+  }
+}
+function send(packet){
+    ws.send(JSON.stringify(packet));
+}
