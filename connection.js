c46143770a40440e8ac70f47f1c3d462b93efcdf
Jade <jade@Jades-MacBook-Pro.local>
2021-03-12 23:11:16 -0500

    connection location modified

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 class Connection{
     constructor(packethandlercallback){
         this.packethandlercallback = packethandlercallback;
-        this.ws = new WebSocket('ws://synosure.ddns.net:8080');
+        this.ws = new WebSocket('wss://jdragon.digital:8080');
         this.ws.onmessage = this.onmessage.bind(this);
         this.ws.onopen = this.onopen.bind(this);
         this.ws.onclose = this.onclose.bind(this);
