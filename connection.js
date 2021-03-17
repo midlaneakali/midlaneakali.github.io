92b0220371dd9022a03c529c6c062ae445ad89d2
Jade <jade@Jades-MacBook-Pro.local>
2021-03-17 14:40:40 -0400

    probably don't need to send pings anymore since timout on server is 0

--- connection.js
+++ connection.js
@@ -6,11 +6,13 @@ class Connection{
         this.ws.onopen = this.onopen.bind(this);
         this.ws.onclose = this.onclose.bind(this);
         this.identifiers = new IdentifierConstants();
+        /*
         setInterval(() => {
             if(this.ws.readyState==1){
-                this.ws.send(true);
+                this.ws.send("1");
             }
         }, 10000);
+        */
     }
     onmessage(evt){
         let packet = evt.data;
